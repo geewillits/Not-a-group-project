110967961266d717394c68030d3300d4da2c1df3
Gregory Willits <dev.gregorywillits@gmail.com>
2021-02-15 03:24:28 +0000

    Initial commit

---
+++ HamiltonWorld/HamiltonGameWelcome.sh
@@ -0,0 +1,6 @@
+#Game based on A.H. book and the musical it inspired.
+#!/bin/bash
+
+echo "How does a bastard, orphan, son of an unlucky woman and a Scotsman, dropped in the middle of a forgotten spot in the Carribean by Providence impoverished in squalor, grow up to be a hero and a scholar?"
+echo "Welcome to the Hamilton game! Where you find out if you can do better than our first Treasury Secretary! Follow the path and try to avoid plagues, wars, temptation and of course Aaron Burr!"
+echo "First thing is first, you need to figure out how to stay alive."
---
+++ HamiltonWorld/StCroix/EarlyChildhood.sh
@@ -0,0 +1,15 @@
+#Growing up on St. Croix, show some options - reading, writing, farming, trading
+
+echo "Bad news Alexander, your mom just died right in front on you from yellow fever. Now you and your brother have to move in with your cousin. Uh-oh, he just committed suicide. What terrible luck!"
+echo "Would you like to get a job with your brother farming on St. Croix? You'd be in charge of a group of slaves and work in the tropical sun for 13 hours a day for very little pay and no prospects. Sound good? (yes or no)"
+
+read Question1
+
+if [ $Question1 = yes ] 
+then
+ echo "Excellent choice, you die at 16 in the fields when the African slaves rise up and rightfully slaughter everyone who kept them in bondage. Game over."
+else 
+ echo "Excellent choice! Search for the hidden file for an alternative path!"
+ fi
+ 
+
